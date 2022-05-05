7316a7b408040c32b4229987f8ae413c437cd721
Justin Chang <changguangyu@yahoo.com>
2022-05-04 22:48:08 -0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { Box, Container, Typography } from '@mui/material';
 import './App.css';
 
 function App() {
+  const [cause, setCause] = useState('cause');
+  const [effect, setEffect] = useState('effect');
+  const [question, setQuestion] = useState('question');
+  const [answer, setAnswer] = useState('answer');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Box style={{ padding: '10vh' }}>
+        <Typography variant="h2" component="h1" style={{ textAlign: 'center' }}>
+          CausalQA Validation
+        </Typography>
+
+        <Typography variant="h5" component="h1" style={{ textAlign: 'center', marginBottom: '1em' }}>
+          Enter to approve, start typing to reject with reason.
+        </Typography>
+
+        <Box>
+          <Typography variant="subtitle1" component="h1" style={{ marginBottom: '0.5em' }}>Cause:</Typography>
+          <Typography variant="body1" component="p" style={{ marginBottom: '0.5em' }}>{cause}</Typography>
+          <Typography variant="subtitle1" component="h1" style={{ marginBottom: '0.5em' }}>Effect:</Typography>
+          <Typography variant="body1" component="p" style={{ marginBottom: '0.5em' }}>{effect}</Typography>
+          <Typography variant="subtitle1" component="h1" style={{ marginBottom: '0.5em' }}>Question:</Typography>
+          <Typography variant="body1" component="p" style={{ marginBottom: '0.5em' }}>{question}</Typography>
+          <Typography variant="subtitle1" component="h1" style={{ marginBottom: '0.5em' }}>Answer:</Typography>
+          <Typography variant="body1" component="p" style={{ marginBottom: '0.5em' }}>{answer}</Typography>
+        </Box>
+
+      </Box>
+    </Container>
   );
 }
 
